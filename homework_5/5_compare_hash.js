e3842b3af742e8e5477f67b6cc23067274dd7cda
Anna Pichuhina <anna.pichuhina@raccoongang.com>
2025-04-09 12:47:36 +0200

    Fix 5_compare_hash

--- homework_5/5_compare_hash.js
+++ homework_5/5_compare_hash.js
@@ -1,15 +1,18 @@
-const crypto = require('crypto');
 const bcrypt = require('bcrypt');
 
-const hash = crypto.createHash('sha256').update('Привіт').digest('hex');
-const userInput = 'Password123';
+const plainPassword = 'Password123';
+const userInput = 'Password';
 
-bcrypt.compare(userInput, hash, (err, result) => {
+bcrypt.hash(plainPassword, 10, (err, hash) => {
     if (err) throw err;
 
-    if (result) {
-        console.log('Пароль правильний!');
-    } else {
-        console.log('Пароль неправильний!');
-    }
+    bcrypt.compare(userInput, hash, (err, result) => {
+        if (err) throw err;
+
+        if (result) {
+            console.log('Пароль правильний!');
+        } else {
+            console.log('Пароль неправильний!');
+        }
+    });
 });
