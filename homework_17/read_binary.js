2fa03550ca00edef0ffe9f6b5b6b5ce5155c96df
Anna Pichuhina <anna.pichuhina@raccoongang.com>
2025-04-20 20:24:45 +0200

    Fix callbacks

--- homework_17/read_binary.js
+++ homework_17/read_binary.js
@@ -5,13 +5,9 @@ const message = 'Hello';
 const binaryData = Buffer.from(message, 'utf8');
 const filePath = path.join(__dirname, 'my-binary-file.bin');
 
-fs.writeFileSync(filePath, binaryData, (err) => {
-    if (err) throw err;
-});
+fs.writeFileSync(filePath, binaryData);
 
-const buffer = fs.readFileSync(filePath, (err, data) => {
-    if (err) throw err;
-});
+const buffer = fs.readFileSync(filePath);
 
 console.log('Вміст файлу у вигляді Buffer:', buffer);
 console.log('Вміст файлу як рядок:', buffer.toString());
